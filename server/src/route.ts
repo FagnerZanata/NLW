db76b6718b7f4a22f59d75b4393f6e9e6f6ae17b
Fagner <fagner_zanata@terceiros.sicredi.com.br>
2020-06-06 21:06:50 -0300

    Aula 2

---
+++ server/src/route.ts
@@ -0,0 +1,21 @@
+import express, { Router } from 'express';
+import knex from './database/connection';
+
+import PointsController from './controllers/PointsController';
+import ItensController from './controllers/ItensController';
+
+
+const routes = express.Router();
+
+const pointsController = new PointsController();
+const itensController = new ItensController();
+
+
+routes.get('/items', itensController.index);
+routes.post('/points', pointsController.create);
+routes.get('/points', pointsController.index);
+routes.get('/points/:id', pointsController.show);
+
+
+
+export default routes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,62 +1,17 @@
-import express, { request, response } from 'express'
+import express from 'express'
+import cors from 'cors';
+import path from 'path';
+import routes from './route';
+
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-
-//rota: endereco
-//Recurso: entidade
-
-//Request param: Parametros na propria rota que identificam um recurso
-// Query param: Parametros no propria rota geralmente opcionais
-
-
-const users = [
-    'Diego',
-    'Rogério',
-    'Maria'
-];
-
-
-app.get('/users', (request, response) => {
-
-    console.log('Listagem de usuários');
-
-    const search = String(request.query.search);
-
-
-    console.log('Search: ' + search);
-
-    const filterduser = search ? users.filter(user => user.includes(search)) : users;
-
-
-    response.json(filterduser);
-});
-
-
-app.get('/users/:id', (request, response) => {
-
-    console.log('Listagem de usuário unico');
-
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users', (request, response) =>{
-    const data = request.body;
-
-    console.log(data);
-    
-    
-    const user = {
-        name: data.name,
-        email: data.email
-    };
-
-    return response.json(user);
-});
+app.use(routes);
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
+
+
+
